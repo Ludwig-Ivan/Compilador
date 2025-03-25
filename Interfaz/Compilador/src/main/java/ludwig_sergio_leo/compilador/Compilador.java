dac3582f535782de9feba3369eefe7a41424d661
Ludwig Ivan <ivansierras11@gmail.com>
2025-03-24 20:50:08 -0600

    Compilador (Interfaz) 0.0.1 MegaAlphaBeta

---
+++ Interfaz/Compilador/src/main/java/ludwig_sergio_leo/compilador/Compilador.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ludwig_sergio_leo.compilador;
+
+/**
+ *
+ * @author PC
+ */
+public class Compilador {
+
+    public static void main(String[] args) {
+        Principal principal=new Principal();
+        
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+}
